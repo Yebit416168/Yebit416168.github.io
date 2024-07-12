dfb3a08bd6c3c8b92e2daed074a13e937a8d2265
Yebit416168 <xx416168ye@outlook.com>
2024-07-12 23:14:35 +0800

    Site updated: 2024-07-12 23:14:34

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720797106750";
+const VERSION = "1720797257084";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
